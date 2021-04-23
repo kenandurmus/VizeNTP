c83c32a4b34659e7fcb3201d0965641730d31361
kenan durmus <kenandurmus715@gmail.com>
2021-04-23 19:24:11 +0300

    Timer Link Button

--- VizeNTP/Form1.cs
+++ VizeNTP/Form1.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace VizeNTP
 {
     public partial class Form1 : Form
     {
+        String hava_durumu_link =  "https://www.mgm.gov.tr/FTPDATA/analiz/sonSOA.xml";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnHavaDurumuBilgiAl_Click(object sender, EventArgs e)
+        {
+            
+            
+                
+         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hava Durumu");
+
         }
     }
 }
